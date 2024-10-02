7ebb04a17261bdde05ce50b14cd49bbb75d1f6ca
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-10-02 15:14:42 -0700

    Create CRT-L01L01-MGMT.sh

---
+++ CRT-L01L01-MGMT.sh
@@ -0,0 +1,13 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update Student Workstation host for the specific lab
+# only if need a simpler /etc/hosts file do the following 2 steps
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/management/hosts
+sudo cp /tmp/hosts                                  /etc/hosts
+
+# update nginx host for the specific lab
+
+#Remove default.conf file since this lab requires only default.bak
+#sudo ssh nginx rm /etc/nginx/conf.d/default.conf
