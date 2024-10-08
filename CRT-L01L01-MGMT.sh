05b6b9a8c0bd0830065656c631e28cfdf9d27673
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-10-08 11:54:12 -0700

    Update CRT-L01L01-MGMT.sh

--- CRT-L01L01-MGMT.sh
+++ CRT-L01L01-MGMT.sh
@@ -5,7 +5,7 @@ PS4='+$(date +"%T.%3N"): '
 # update Student Workstation host for the specific lab
 # only if need a simpler /etc/hosts file do the following 2 steps
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/Management/hosts
-sudo scp /tmp/hosts                                  /etc/hosts
+sudo scp /tmp/hosts    nginx:/etc/hosts
 
 # update nginx host for the specific lab
 
