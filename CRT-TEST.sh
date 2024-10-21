a315b0db5f66dc50fb8dcd5053a18a733aa0f72c
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-10-21 10:37:43 -0700

    Update CRT-TEST.sh

--- CRT-TEST.sh
+++ CRT-TEST.sh
@@ -7,6 +7,10 @@ PS4='+$(date +"%T.%3N"): '
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/Management/hosts
 sudo scp /tmp/hosts    nginx:/etc/hosts
 
+# For ConfigKnowledge Lab (Exam 2)
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigKnowledge/lb.orig
+sudo scp /tmp/lb.orig    nginx:/etc/nginx/conf.d/lb.orig
+
 # update nginx host for the specific lab
 
 #Remove default.conf file since this lab requires only default.bak
