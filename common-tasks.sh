b51d0b9d37f9edd4c5c70b1918ae688fd3136e1a
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-01-24 10:32:31 -0800

    Update common-tasks.sh
    changed script back to unique lab number lab-info.md

--- common-tasks.sh
+++ common-tasks.sh
@@ -4,7 +4,7 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 # get lab-info.md for student guide bravais id
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab-info-MGMT.md --output /tmp/lab-info.md
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab-info.md --output /tmp/lab-info.md
 brav_id=$(awk -F '|' "/$LAB_ID/"' {print $2}' /tmp/lab-info.md)
 
 # install student guide
