02ca18281ad580ce49dc8e7ca0eceae23f5c4838
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-01-27 12:40:48 -0800

    Update CRT-TEST.sh

--- CRT-TEST.sh
+++ CRT-TEST.sh
@@ -11,7 +11,16 @@ sudo scp /tmp/hosts    nginx:/etc/hosts
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigKnowledge/lb.orig
 sudo scp /tmp/lb.orig    nginx:/etc/nginx/conf.d/lb.orig
 
-# update nginx host for the specific lab
+# For ConfigDemonstrate Lab (Exam 3)
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/juice.conf
+sudo scp /tmp/juice.conf    nginx:/etc/nginx/conf.d/juice.conf
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/ssl_params.conf
+sudo scp /tmp/ssl_params.conf    student:/etc/nginx/conf.d/juice.conf
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/{curl,create_certs}.sh
+sudo ssh student mkdir /home/student/ssl
+sudo scp /tmp/{curl,create_certs}.sh    student:/home/student/ssl/
 
 #Remove default.conf file since this lab requires only default.bak
 #sudo ssh nginx rm /etc/nginx/conf.d/default.conf
