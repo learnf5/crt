6e395a40e14369d844daf3cfa86264ef8c0dd799
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-02-10 10:23:27 -0800

    Update CRT-TEST.sh

--- CRT-TEST.sh
+++ CRT-TEST.sh
@@ -8,26 +8,26 @@ curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.
 sudo scp /tmp/hosts    nginx:/etc/hosts
 
 # For ConfigKnowledge Lab (Exam 2)
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigKnowledge/lb.orig
-sudo scp /tmp/lb.orig    nginx:/etc/nginx/conf.d/lb.orig
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigKnowledge/lb.orig
+# sudo scp /tmp/lb.orig    nginx:/etc/nginx/conf.d/lb.orig
 
 # For ConfigDemonstrate Lab (Exam 3)
-sudo ssh nginx mkdir --parents /etc/nginx/ssl
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/juice.conf
-sudo scp /tmp/juice.conf    nginx:/etc/nginx/conf.d/juice.conf
+# sudo ssh nginx mkdir --parents /etc/nginx/ssl
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/juice.conf
+# sudo scp /tmp/juice.conf    nginx:/etc/nginx/conf.d/juice.conf
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/ssl_params.conf
-sudo ssh nginx mkdir --parents /etc/nginx/ssl-configs
-sudo scp /tmp/ssl_params.conf    nginx:/etc/nginx/ssl-configs/ssl-params.conf
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/ssl_params.conf
+# sudo ssh nginx mkdir --parents /etc/nginx/ssl-configs
+# sudo scp /tmp/ssl_params.conf    nginx:/etc/nginx/ssl-configs/ssl-params.conf
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/{curl,create_certs}.sh
-sudo ssh nginx mkdir --parents /home/student/ssl
-sudo scp /tmp/{curl,create_certs}.sh    nginx:/home/student/ssl/
-sudo ssh nginx chmod +x /home/student/ssl/{create_certs,curl}.sh
-sudo ssh nginx chown -R student.student /home/student/ssl
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/{curl,create_certs}.sh
+# sudo ssh nginx mkdir --parents /home/student/ssl
+# sudo scp /tmp/{curl,create_certs}.sh    nginx:/home/student/ssl/
+# sudo ssh nginx chmod +x /home/student/ssl/{create_certs,curl}.sh
+# sudo ssh nginx chown -R student.student /home/student/ssl
 
 #Remove default.conf file since this lab requires only default.bak
-sudo ssh nginx mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
+#sudo ssh nginx mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
 
 
 # For Troubleshooting Lab (Exam 4)
@@ -41,3 +41,12 @@ sudo scp /tmp/ssl_params.conf    nginx:/etc/nginx/ssl-configs/ssl-params.conf
 
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/Troubleshooting/juice.tls
 sudo scp /tmp/juice.tls    nginx:/etc/nginx/conf.d/juice.tls
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/ConfigDemonstrate/{curl,create_certs}.sh
+sudo ssh nginx mkdir --parents /home/student/ssl
+sudo scp /tmp/{curl,create_certs}.sh    nginx:/home/student/ssl/
+sudo ssh nginx chmod +x /home/student/ssl/{create_certs,curl}.sh
+sudo ssh nginx chown -R student.student /home/student/ssl
+
+#Remove default.conf file since this lab requires only default.bak
+sudo ssh nginx mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
