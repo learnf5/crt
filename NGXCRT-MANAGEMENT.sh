569f97adda124b517bc3a5112e6ff371078fe9c9
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-02-10 14:35:30 -0800

    Create NGXCRT-MANAGEMENT.sh

---
+++ NGXCRT-MANAGEMENT.sh
@@ -0,0 +1,9 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update Student Workstation host for the specific lab
+# only if need a simpler /etc/hosts file do the following 2 steps
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/crt/main/Management/hosts
+sudo scp /tmp/hosts    nginx:/etc/hosts
+
